e98ec38fa175f86bca7d04fa97e3bacbb2088035
Evan <evan@funmula.com>
2020-06-02 16:20:56 +0800

    fix text color in dark mode

--- DTActionSheet iOS Example/ViewController.swift
+++ DTActionSheet iOS Example/ViewController.swift
@@ -10,23 +10,24 @@ import UIKit
 import DTActionSheet
 
 class ViewController: UIViewController {
-  
-  private let formatter = DateFormatter()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-    formatter.dateStyle = .short
-  }
-
-  @IBAction func changeDate(_ sender: UIButton) {
-    let sheet = DTDatePickerSheet(style: .dark)
-    sheet.setTitle("Choose Date")
-    sheet.configDatePicker(mode: .date, date: Date()) { [unowned self] date in
-      let title = self.formatter.string(from: date)
-      sender.setTitle(title, for: .normal)
+    private let formatter = DateFormatter()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        formatter.dateStyle = .short
     }
-    sheet.show()
-  }
-  
+    
+    @IBAction func changeDate(_ sender: UIButton) {
+        let sheet = DTDatePickerSheet(style: .dark)
+        sheet.setTitle("Choose Date")
+        
+        sheet.configDatePicker(mode: .date, date: Date()) { [unowned self] date in
+            let title = self.formatter.string(from: date)
+            sender.setTitle(title, for: .normal)
+        }
+        sheet.show()
+    }
+    
 }
--- Sources/DTDatePickerSheet.swift
+++ Sources/DTDatePickerSheet.swift
@@ -8,59 +8,61 @@
 import UIKit
 
 public final class DTDatePickerSheet: DTSavableActionSheet {
-
-  public typealias Action = (Date) -> Void
-  
-  public override var contentViewHeight: CGFloat {
-    return 260
-  }
-  
-  private let datePicker = UIDatePicker()
-  private var action: Action?
-  
-  public required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  public override init(style: DTActionSheetStyle) {
-    super.init(style: style)
     
-    contentView.backgroundColor = .white
+    public typealias Action = (Date) -> Void
     
-    dismissButton.tintColor = UIColor(red:0.26, green:0.26, blue:0.26, alpha:1.00)
-    saveButton.tintColor = UIColor(red:0.26, green:0.26, blue:0.26, alpha:1.00)
-    saveButton.addTarget(self, action: #selector(save), for: UIControlEvents.touchUpInside)
-    
-    layoutDatePicker()
-  }
-  
-  @objc func save() {
-    cancel()
-    if let action = action {
-      action(datePicker.date)
+    public override var contentViewHeight: CGFloat {
+        return 260
     }
-  }
-  
-  public func configDatePicker(mode: UIDatePickerMode, date: Date?, minimumDate: Date? = nil, maximumDate: Date? = nil, action: Action?) {
-    datePicker.datePickerMode = mode
-    datePicker.minimumDate = minimumDate
-    datePicker.maximumDate = maximumDate
-    self.action = action
-  }
-  
-  // MARK: - Private
-  
-  private func layoutDatePicker() {
-    datePicker.datePickerMode = .date
     
-    contentView.addSubview(datePicker)
+    private let datePicker = UIDatePicker()
+    private var action: Action?
     
-    datePicker.translatesAutoresizingMaskIntoConstraints = false
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
-    let bottom = NSLayoutConstraint(item: datePicker, attribute: .bottom, relatedBy: .equal, toItem: contentView, attribute: .bottom, multiplier: 1, constant: 0)
-    let centerX = NSLayoutConstraint(item: datePicker, attribute: .centerX, relatedBy: .equal, toItem: contentView, attribute: .centerX, multiplier: 1, constant: 0)
+    public override init(style: DTActionSheetStyle) {
+        super.init(style: style)
+        
+        contentView.backgroundColor = .white
+        
+        dismissButton.tintColor = UIColor(red:0.26, green:0.26, blue:0.26, alpha:1.00)
+        saveButton.tintColor = UIColor(red:0.26, green:0.26, blue:0.26, alpha:1.00)
+        saveButton.addTarget(self, action: #selector(save), for: UIControlEvents.touchUpInside)
+        
+        layoutDatePicker()
+    }
     
-    contentView.addConstraints([bottom, centerX])
-  }
-
+    @objc func save() {
+        cancel()
+        if let action = action {
+            action(datePicker.date)
+        }
+    }
+    
+    public func configDatePicker(mode: UIDatePickerMode, date: Date?, minimumDate: Date? = nil, maximumDate: Date? = nil, backgroundColor: UIColor = .clear, textColor: UIColor = .darkText, action: Action?) {
+        datePicker.datePickerMode = mode
+        datePicker.minimumDate = minimumDate
+        datePicker.maximumDate = maximumDate
+        datePicker.backgroundColor = backgroundColor
+        datePicker.setValue(textColor, forKey: "textColor")
+        self.action = action        
+    }
+    
+    // MARK: - Private
+    
+    private func layoutDatePicker() {
+        datePicker.datePickerMode = .date
+        
+        contentView.addSubview(datePicker)
+        
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        
+        let bottom = NSLayoutConstraint(item: datePicker, attribute: .bottom, relatedBy: .equal, toItem: contentView, attribute: .bottom, multiplier: 1, constant: 0)
+        let centerX = NSLayoutConstraint(item: datePicker, attribute: .centerX, relatedBy: .equal, toItem: contentView, attribute: .centerX, multiplier: 1, constant: 0)
+        
+        contentView.addConstraints([bottom, centerX])
+    }
+    
 }
